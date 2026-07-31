d9d166d05405dcba1dbe786b45618ad2d40b99c6
Islem <bboyblack2005@gmail.com>
2026-07-31 21:08:57 +0200

    finishing notes

--- notes.js
+++ notes.js
@@ -13,18 +13,36 @@ function loadNotes(){
     return savedNotes ? JSON.parse(savedNotes) : []
 }
 
-function saveNote(event){
-    event.preventDefault()
+function saveNote(event) {
+    event.preventDefault();
 
     const title = document.getElementById('noteTitle').value.trim();
     const content = document.getElementById('noteContent').value.trim();
 
-    notes.unshift({
-        id: generateId(),
-        title: title,
-        content: content
-    });
+    if (editingNoteId === null) {
+
+        
+        notes.unshift({
+            id: generateId(),
+            title: title,
+            content: content
+        });
+
+    } else {
+
+        
+        const note = notes.find(note => note.id === editingNoteId);
+
+        note.title = title;
+        note.content = content;
+
+        editingNoteId = null;
+    }
+
+    saveNotes();
     renderNotes();
+    closeNoteDialog();
+    document.getElementById('noteForm').reset();
 }
 
 function generateId(){
@@ -35,15 +53,36 @@ function saveNotes(){
     localStorage.setItem('quickNotes', JSON.stringify(notes))
 }
 
+let editingNoteId = null;
+function editNote(id) {
+    const note = notes.find(note => note.id === id);
+
+    document.getElementById("noteTitle").value = note.title;
+    document.getElementById("noteContent").value = note.content;
+
+    editingNoteId = id;
+
+    openNoteDialog();
+    
+}
+
 function renderNotes(){
     const notesContainer = document.getElementById('notesContainer');
 
     notesContainer.innerHTML = notes.map(note =>`
         <div class="note-card">
+            <button class="edit-btn" onclick="editNote('${note.id}')">Edit</button>
+            <button class="delete-btn" onclick="deleteNote('${note.id}')">✕</button>
             <h3 class="note-title">${note.title}</h3>
             <p class="note-content">${note.content}</p>
         </div>`
     ).join('')
+}
+function deleteNote(id) {
+    notes = notes.filter(note => note.id !== id);
+
+    saveNotes();
+    renderNotes();
 }
 
 function openNoteDialog() {
